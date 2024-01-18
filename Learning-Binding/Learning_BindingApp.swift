ac0f2a8b12be115134667ed412bfd97ce5f021ef
Anand Narayan <Anand.Narayan@gov.ab.ca>
2024-01-18 06:15:36 -0700

    Initial Commit

---
+++ Learning-Binding/Learning_BindingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_BindingApp.swift
+//  Learning-Binding
+//
+//  Created by Anand Narayan on 2024-01-18.
+//
+
+import SwiftUI
+
+@main
+struct Learning_BindingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
